19f4e518ebe4e1630d83cad583e1b3def04825ca
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2022-11-15 14:38:59 +0100

    Ajout de la route GET et POST pour une sauce

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -1,28 +1,38 @@
 const Sauce = require('../models/sauce');
 
 exports.createSauce = (req, res, next) => {
-  console.log(req);
-    const sauce = new Sauce({      
-      name: req.body.name,
-      manufacturer: req.body.manufacturer,
-      description: req.body.description,
-      imageUrl: req.body.imageUrl,
-      mainPepper: req.body.mainPepper,
-      heat: req.body.heat,
-      userId: req.auth.userId
-    });
-    sauce.save().then(
-      () => {
-        res.status(201).json({
-          message: 'Sauce saved successfully!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error,
-          message: 'Ne peut pas être enregistré dans la BD'          
-        })
-      }
-    );
-  };
+  console.log("req =>", req.body.sauce);
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+  });
+  sauce.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Sauce saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error,
+        message: 'Ne peut pas être enregistré dans la BD'          
+      })
+    }
+  );
+};
+
+exports.getAllSauces = (req, res, next) => {
+  Sauce.find()
+  .then(sauces => {
+      if(!sauces){
+          return res.status(404).json({error : "No results found"});
+      };
+      res.status(200).json(sauces);
+  })
+  .catch(error => {
+      res.status(500).json({error});
+  });
+};
