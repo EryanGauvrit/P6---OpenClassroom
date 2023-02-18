49c0baa4b7ad5e5d9978f408b47ce8aebfbaeebd
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2023-02-18 20:43:04 +0100

    corrections

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -20,12 +20,12 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
        .then(user => {
            if (!user) {
-               return res.status(401).json({ message: "L'email ou le mot de passe est incorrect"});
+               return res.status(401).json({ message: "L'email ou le mot de passe est incorrect !"});
            }
            bcrypt.compare(req.body.password, user.password)
                .then(valid => {
                    if (!valid) {
-                       return res.status(401).json({ message: "L'email ou le mot de passe est incorrect"});
+                       return res.status(401).json({ message: "L'email ou le mot de passe est incorrect !"});
                    }
                    res.status(200).json({
                        userId: user._id,
